06c3a236437e272e9c9c575401814da230f4d17b
Aniruddhalolas <80830063+Aniruddhalolas@users.noreply.github.com>
2021-05-30 18:56:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+prediction = ""
+
+Webcam.set({
+    width: 350,
+    height:300,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function (data_url){
+        document.getElementById("result").innerHTML= '<img id="captured_image" src="'+data_url+'">';
+    });
+}
+
+console.log('ml5version: ', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ZrlW6qeAF/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log(' Model Loaded!');
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The Prediction is " + prediction;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1+ speak_data_2);
+    synth.speak(utterThis);
+}
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+     if(error){
+         console.error(error);
+     }
+     else{
+         console.log(results);
+         document.getElementById("result_emotion_name").innerHTML = results[0].label;
+         document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+         prediction_ = results[0].label;
+
+         speak();
+         if(results[0].label == "amazing"){
+             document.getElementById("update_emoji").innerHTML = "&#128076;";
+         }
+         if(results[0].label == "best"){
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "victory"){
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+     }
+}
